2b0237d64171b8d046875171d37fb95296ebbd86
Hipnos-J <josuexo902@gmail.com>
2024-07-15 19:53:56 -0400

    first commit

---
+++ assets/app.js
@@ -0,0 +1,119 @@
+const encriptar = document.getElementById("button__encriptar");
+const desencriptar = document.getElementById("button__desencriptar");
+const copy = document.getElementById("button__copiar");
+const textoInicial = document.getElementById("textoInput");
+const textFinal = document.getElementById("textoFinal");
+const munheco = document.getElementById("munheco");
+const textInfo = document.getElementById("textoInfo");
+const rigth = document.getElementById("rigth")
+	
+const remplace = (newvalue) => {
+	textFinal.innerHTML = newvalue;
+	textFinal.classList.add("ajustar");
+	rigth.classList.add("ajuste")
+	textoInicial.value = "";
+	textoInicial.style.height = "auto"
+	textoInicial.placeholder = "Ingrese el texto aquí";
+	munheco.classList.add("ocultar");
+	textInfo.classList.add("ocultar");
+	copy.classList.remove("bn_ocultar");
+}
+
+const reset = () => {
+	textoInicial.value = "";
+    textoInicial.style.height = "auto";
+	textFinal.innerHTML = "";
+	rigth.classList.remove("ajuste")
+	textFinal.classList.remove("ajustar");
+	munheco.classList.remove("ocultar");
+	textFinal.placeholder = "Ningún mensaje fue encontrado";
+	textInfo.classList.remove("ocultar")
+	copy.classList.add("bn_ocultar");
+	textoInicial.focus();
+};
+
+let remplazar = [
+	["e", "enter"],
+	["o", "ober"],
+	["i", "imes"],
+	["a", "ai"],
+	["u", "ufat"]
+];
+
+function validarMinuscula(event) {
+	const tecla = event.key;
+	const esMinuscula = /[a-z]/.test(tecla);
+	if (!esMinuscula && tecla !== "Backspace") {
+	  event.preventDefault();
+	  swal({
+		text:"Recuerde que es solo en minusculas",
+		icon: "warning"});
+	}
+}
+
+encriptar.addEventListener('click', () => {
+
+	const texto = textoInicial.value.toLowerCase();
+
+	if (texto != "") {
+		function encript(newtext) {
+			for (let i = 0; i < remplazar.length; i++) {
+				if (newtext.includes(remplazar[i][0])) {
+					newtext = newtext.replaceAll(remplazar[i][0], remplazar[i][1]);
+				};
+			};
+			return newtext;
+		};
+		remplace(encript(texto));
+	} else {
+		swal({
+      text:"Ingrese texto a encriptar",
+      icon: "warning"});
+		reset();
+	};
+});
+
+desencriptar.addEventListener('click', () => {
+
+	const texto = textoInicial.value.toLowerCase();
+
+	if (texto != "") {
+		function desencript(newtext) {
+			for (let i = 0; i < remplazar.length; i++) {
+				if (newtext.includes(remplazar[i][1])) {
+					newtext = newtext.replaceAll(remplazar[i][1], remplazar[i][0]);
+				};
+			};
+			return newtext;
+		};
+		remplace(desencript(texto));
+	} else {
+		swal({
+      text:"Ingrese texto a desencriptar",
+      icon: "warning"});
+		reset();
+	};
+});
+
+copy.addEventListener("click", () => {
+	let texto = textFinal;
+	texto.select();
+	texto.setSelectionRange(0, 99999);
+  //https://stackoverflow.com/questions/60581285/execcommand-is-now-obsolete-whats-the-alternative
+  	navigator.clipboard.writeText(texto.value);
+	swal({
+		text:"Texto Copiado",
+		icon: "success"});
+	reset();
+});
+//auto ajuste de textarea
+textoInicial.addEventListener("change", e => {
+	textoInicial.style.height = "auto";
+	let scHeight = e.target.scrollHeight;
+	textoInicial.style.height = `${scHeight}px`;
+});
+textoInicial.addEventListener("keyup", e => {
+	textoInicial.style.height = "auto";
+	let scHeight = e.target.scrollHeight;
+	textoInicial.style.height = `${scHeight}px`;
+});
